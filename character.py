498d08791b418450b40b5fa86741f44d1802ef04
mohitgarg <mohitgar2001@gmail.com>
2021-10-27 20:39:08 -0500

    Added function to print out sentence describing character

--- character.py
+++ character.py
@@ -11,22 +11,24 @@ def random_character_name():
     title = res.get('name').get('title')
     first = res.get('name').get('first')
     last = res.get('name').get('last')
-    print(title + ' ' + first + ' ' + last)
+    return title + '.' + ' ' + first + ' ' + last
 
 def random_character_gender():
     gender = res.get('gender')
-    print(gender)
+    return gender
 
 def random_character_home():
     home = res.get('location').get('country')
-    print(home)
+    return home
 
 def random_character_age():
     age = res.get('dob').get('age')
-    print(age)
+    return age
 
 def random_character_picture():
     picture = res.get('picture').get('large')
     return picture
+def random_character_paragraph():
+    print( "{} is a {} year old {} from {}.".format(random_character_name(), random_character_age(),random_character_gender(),random_character_home()))
 
 
--- routes.py
+++ routes.py
@@ -2,14 +2,15 @@ from flask import render_template
 from . import app
 from .character import random_character, random_character_name, random_character_gender
 from .character import random_character_home, random_character_age
+from .character import random_character_paragraph
 
 
 # Default route
 @app.route('/')
 def index():
-    random_character()
     random_character_name()
     random_character_gender()
     random_character_home()
     random_character_age()
+    random_character_paragraph()
     return render_template('index.html')
