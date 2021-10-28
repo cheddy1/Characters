7541f49ef26cbe8a2101b9d1ae580883552e8f38
Peter Gessler <Pngessler@ku.edu>
2021-10-27 20:07:22 -0500

    Added functions that print characters name, gender, home, and age to terminal

--- character.py
+++ character.py
@@ -1,5 +1,32 @@
 import requests
 
+res = requests.get("https://randomuser.me/api/").json().get('results')[0]
 def random_character():
-    res = requests.get("https://randomuser.me/api/")
-    print(res.json())
+    # res = requests.get("https://randomuser.me/api/").json().get('results')[0]
+    result = res
+
+    print(result)
+
+def random_character_name():
+    title = res.get('name').get('title')
+    first = res.get('name').get('first')
+    last = res.get('name').get('last')
+    print(title + ' ' + first + ' ' + last)
+
+def random_character_gender():
+    gender = res.get('gender')
+    print(gender)
+
+def random_character_home():
+    home = res.get('location').get('country')
+    print(home)
+
+def random_character_age():
+    age = res.get('dob').get('age')
+    print(age)
+
+def random_character_picture():
+    picture = res.get('picture').get('large')
+    return picture
+
+
--- routes.py
+++ routes.py
@@ -1,8 +1,15 @@
 from flask import render_template
 from . import app
-from .character import random_character
+from .character import random_character, random_character_name, random_character_gender
+from .character import random_character_home, random_character_age
+
+
 # Default route
 @app.route('/')
 def index():
     random_character()
+    random_character_name()
+    random_character_gender()
+    random_character_home()
+    random_character_age()
     return render_template('index.html')
