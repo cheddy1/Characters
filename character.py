9ffd61db36345177a055f07970587e9425301674
HuntDubs <Hunterwilson4092@gmail.com>
2021-11-18 11:37:55 -0600

    Comments

--- character.py
+++ character.py
@@ -3,16 +3,21 @@ from flask import request
 import random
 import re
 
+# Date: 11/18/21
+# Authors: EECS 448 Group 15
+# Description: Functions to retrieve and evaluate random character information
+
 UI_API_1 = '241E1EB8-FFA54369-A0851285-2C688163'
 UI_API_2 = 'A3D1D37D-506D4D4B-B1C36F96-5FF139DB'
 access_token = "?access_token=CWBwf6Bb0yL5QeJcefGLk9F2JLjnDt91Q9QNBHNC-gcKsW0kibHga6BuPz2No7wV";
 API_Song = "https://api.genius.com/songs/"
 
-
+# @post Uses randomuser api to get a random user
 def random_character():
     return requests.get("https://randomuser.me/api/").json().get('results')[0]
 
-
+# @param uses global variables: access_token, API_Song
+# @post Generates a random song using the genius.com api
 def random_song():
     song_id = str(random.randint(1, 2500000))
     res = requests.get(API_Song + song_id + access_token)
@@ -22,7 +27,10 @@ def random_song():
     song = res.json()['response']['song']
     return song['title'] + ' by ' + song['primary_artist']['name']
 
-
+# @param age is the characters age
+# @param gender is the characters gender
+# @param key is the APIkey needed to use the API
+# @post generates a random picture based on age and gender
 def character_picture(age, gender, key):
     uiface = requests.get('https://uifaces.co/api', headers={
         'X-API-KEY': key,
@@ -36,7 +44,7 @@ def character_picture(age, gender, key):
     )
     return uiface.json()
 
-
+# @post reads from a txt file of hobbies and returns 3 random ones
 def character_hobbies_young():
     hobbies = open('static/hobbies.txt').read().splitlines()
     hobby1 = random.choice(hobbies)
@@ -48,7 +56,7 @@ def character_hobbies_young():
     else:
         return character_hobbies_young()
 
-
+# @post reads from a txt file of hobbies for older people and returns 3 random ones
 def character_hobbies_old():
     hobbiesold = open('static/hobbiesold.txt').read().splitlines()
     hobby1 = random.choice(hobbiesold)
@@ -60,12 +68,13 @@ def character_hobbies_old():
     else:
         return character_hobbies_old()
 
-
+# @post returns a random poem from the bean poems API
 def character_poem():
     fav_poem = requests.get('https://www.beanpoems.com/api/poems/random').json().get('body')
     return fav_poem
 
-
+# @param one sentence to start the deepai text generator
+# @post returns ai generated text based on the characters name age and gender
 def character_bio(text):
     r = requests.post(
         "https://api.deepai.org/api/text-generator",
@@ -76,33 +85,39 @@ def character_bio(text):
     )
     return ' '.join(re.split(r'(?<=[.:;])\s', r.json()['output'])[:4])
 
-
+# @param a character retrieved from random_character() using the randomuser API
+# @post return a random character name
 def random_character_name(character):
     first = character.get('name').get('first')
     last = character.get('name').get('last')
     return first + ' ' + last
 
-
+# @param a character retrieved from random_character() using the randomuser API
+# @post return a random character gender
 def random_character_gender(character):
     gender = character.get('gender')
     return gender
 
-
+# @param a character retrieved from random_character() using the randomuser API
+# @post return a random character country of origin
 def random_character_home(character):
     home = character.get('location').get('country')
     return home
 
-
+# @param a character retrieved from random_character() using the randomuser API
+# @post return a random character age
 def random_character_age(character):
     age = character.get('dob').get('age')
     return age
 
-
+# @param a character retrieved from random_character() using the randomuser API
+# @post return a random character picture
 def random_character_picture(character):
     picture = character[0]['photo']
     return picture
 
-
+# @pre a form using the POST method collects user input in customization.html
+# @post returns user provided name, country, age, and/or gender if input is valid
 def customization_info():
     if request.method == "POST":
         output = request.form.to_dict()
@@ -130,7 +145,7 @@ def customization_info():
 
         return[name, country, age, gender]
 
-
+# @post returns all information about the random character
 def random_character_paragraph():
     character = random_character()
     if request.method == "POST":
@@ -177,7 +192,7 @@ def random_character_paragraph():
     song = random_song()
     return [text, picture, name, home, age, gender, poem, hobbiestext, song]
 
-
+# @post returns a random character picture
 def random_character_link():
     character = random_character()
     picture = random_character_picture(character)
--- routes.py
+++ routes.py
@@ -3,13 +3,17 @@ from . import app
 from .character import random_character_paragraph, customization_info, random_song
 from .tests import test1, test2, test3, test4, test5
 
+# Date: 11/18/21
+# Authors: EECS 448 Group 15
+# Description: All of the routes of the web application
+
 
 # Default route
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+# Random Character Generation Route
 @app.route('/generator',methods = ['POST',"GET"])
 def generator():
     character = random_character_paragraph()
@@ -17,17 +21,17 @@ def generator():
                            data3=character[2], data4=character[3], data5=character[4],
                            data6=character[5], data7=character[6], data8=character[7], song=character[8])
 
-
+# Information Page Route
 @app.route('/information', methods = ['POST','GET'])
 def information():
     return render_template('information.html')
 
-
+# Customization Page Route
 @app.route('/customization', methods = ['POST','GET'])
 def customization():
     return render_template('customization.html')
 
-
+# Test page Route
 @app.route('/test', methods = ['POST','GET'])
 def test():
     character = random_character_paragraph()
@@ -44,7 +48,8 @@ def test():
 
     return render_template('test.html', test1=testone, test2=testtwo, test3=testthree, test4=testthree, test5=testfive)
 
-
+# Post user input Customization page route
+# (goes to this route after entering custom user information, before character generation)
 @app.route('/custom_process_test', methods=["POST","GET"])
 def custom_process_test():
     custom_info = customization_info()
